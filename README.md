e965bc0c26d9ae1956fb09bea9f50ddd9fa3aed4
Jose Christian <j.christian@brighton.ac.uk>
2022-06-02 08:54:06 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# sfdt - simple and functional data tools
+
+A simple set of tools to help manage datasets. Specifically made for the cross-comm project.
+
+---
+
+## listDates
+
+    listDates(startdate, enddate) # yyyy-mm-dd format
+
+## count values
+
+    countVals([rawlist]) # returns a list of dicts
---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='sfdt',
+      description='A simple set of tools to help manage datasets. Specifically made for the cross-comm project.',
+      version='0.0.1',
+      author='Jose Christian',
+      url='https://github.com/drjchris/sfdt',
+      license='Apache 2.0',
+      py_modules=['sfdt'],
+      )
